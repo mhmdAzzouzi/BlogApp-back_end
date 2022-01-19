f4711b8c259a348fc41d7914275282c0a7d42d06
mhmdAzzouzi <mhmdazzouzi1999@gmail.com>
2022-01-19 14:51:10 +0200

    added Password hashing, Auth Middleware & auth api

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,17 @@
 var express = require('express');
 var router = express.Router();
-
+const {authenticateToken} = require('../middleware/auth')
 const userController = require('../controllers').user;
-
+const authController = require('../controllers').auth
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.status(200).send("The Blog API !!!!!!!!!!!");
 });
+/*Auth router */
+router.post('/api/auth/login' , authController.login  )
 
 /* User Router */
-router.get('/api/user', userController.list);
+router.get('/api/user',  userController.list);
 router.get('/api/user/:id', userController.getById);
 router.post('/api/user', userController.add);
 router.put('/api/user/:id', userController.update);
