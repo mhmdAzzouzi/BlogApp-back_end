27b957611f5929f381db4ac14e9f7526f709bdd3
mhmdAzzouzi <mhmdazzouzi1999@gmail.com>
2022-01-22 17:14:51 +0200

    created validation schema for Users

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,7 @@
 const { User } = require("../models");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const { userSchema, updateUserSchema } = require("../utils/validation_schema");
 
 const list = async (req, res) => {
   try {
@@ -23,8 +24,18 @@ const getById = async (req, res) => {
 
 const add = async (req, res) => {
   try {
-    let { firstName, lastName, email, password, verified, profileImage } =
-      req.body;
+    let {
+      firstName,
+      lastName,
+      email,
+      password,
+      confirmPassword,
+      verified,
+      profileImage,
+    } = req.body;
+
+    let validate = await userSchema.validateAsync(req.body);
+
     let findUser = await User.findOne({ where: { email } });
 
     if (findUser)
@@ -49,13 +60,16 @@ const add = async (req, res) => {
       return res.status(200).send({ user, token });
     }
   } catch (error) {
-    return res.status(400).send(error);
+    if (error.isJoi == true)
+      return res.status(422).send({ message: "invalid input" });
+    return res.status(500).send(error);
   }
 };
 
 const update = async (req, res) => {
   try {
     let { firstName, lastName, email } = req.body;
+    const validate = await updateUserSchema.validateAsync(req.body);
     const user = await User.findOne({ where: { uuid: req.params.uuid } });
     if (!user) {
       return res.status(404).send({
@@ -69,6 +83,8 @@ const update = async (req, res) => {
     });
     return res.status(200).send(user);
   } catch (error) {
+    if (error.isJoi)
+      return res.status(422).send({ error, message: "invalid input" });
     return res.status(400).send(error);
   }
 };
