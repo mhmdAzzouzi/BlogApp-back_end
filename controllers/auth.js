b28155c4ab9823202bca7e363f422e6c64c574bc
mhmdAzzouzi <mhmdazzouzi1999@gmail.com>
2022-01-23 13:41:16 +0200

    fixed error response bug

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,19 +2,21 @@ const jwt = require("jsonwebtoken");
 const { User } = require("../models");
 const bcrypt = require("bcrypt");
 require("dotenv").config();
+const {loginSchema} = require("../utils/validation_schema")
 
 const login = async (req, res) => {
   try {
     let { email, password } = req.body;
-    const user = await User.findOne({ where: { email } });
+    let validation= await loginSchema.validateAsync(req.body)
 
+    const user = await User.findOne({ where: { email } });
     if (!user)
-      return res.status(404).send({ message: "incorrect email or password" });
+      return res.status(404).send({ message: "Incorrect email or password" });
 
     let checkPassword = await bcrypt.compare(password, user.password);
 
     if (!checkPassword)
-      return res.status(404).send({ message: "incorrect email or password" });
+      return res.status(404).send({ message: "Incorrect email or password" });
 
     const token = jwt.sign(
       { uuid: user.uuid , firstName: user.firstName, email: user.email , verified: user.verified},
@@ -24,8 +26,11 @@ const login = async (req, res) => {
 
     return res.status(200).send({ user, token });
   } catch (error) {
+    if(error.isJoi){
+      return res.status(400).send({message: "Please enter a valid email"})
+    }
     return res
-      .status(400)
+      .status(500)
       .send({ message: "something went wrong :( " } + error);
   }
 };
